e4942a9980513102f190e3d5d12a5a17742997c9
Lorenzo Durante <103606632+LorenzoEU@users.noreply.github.com>
2025-05-03 21:33:16 +0200

    Update train.py

--- train.py
+++ train.py
@@ -2,17 +2,15 @@
     REINFORCE and Actor-critic algorithms
 """
 import argparse
-
 import torch
 import gym
-
+import pickle
 from env.custom_hopper import *
 from agent import Agent, Policy
 
-
 def parse_args():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--n-episodes', default=50000, type=int, help='Number of training episodes')
+	parser.add_argument('--n-episodes', default=20000, type=int, help='Number of training episodes')
 	parser.add_argument('--print-every', default=500, type=int, help='Print info every <> episodes')
 	parser.add_argument('--update-every', default=10, type=int, help='Update policy every <> episodes')
 	parser.add_argument('--device', default='cpu', type=str, help='network device [cpu, cuda]')
@@ -33,19 +31,13 @@ def main():
 	print('Dynamics parameters:', env.get_parameters())
 	print(args.actor_critic)
 	print(args.baseline)
-
-	"""
-		Training
-	"""
 	observation_space_dim = env.observation_space.shape[-1]
 	action_space_dim = env.action_space.shape[-1]
 
 	policy = Policy(observation_space_dim, action_space_dim)
 	agent = Agent(policy, device=args.device, baseline = args.baseline, actor_critic=args.actor_critic)
+	episodes_returns = []
 
-    #
-    # TASK 2 and 3: interleave data collection to policy updates
-    #
 
 	for episode in range(args.n_episodes):
 		done = False
@@ -57,27 +49,32 @@ def main():
 				action, action_probabilities, value = agent.get_action(state)
 			else:
 				action, action_probabilities, _ = agent.get_action(state)
+				value = None
 			
 			new_state, reward, done, info = env.step(action.detach().cpu().numpy())
+
 			if args.actor_critic:
 				if not done:
 					_, _, next_value = agent.get_action(new_state, evaluation=True)
 				else:
-					next_value = torch.tensor(0.0, device=args.device)
+					next_value = torch.tensor(0.0, device=args.device).unsqueeze(0)
 				agent.store_outcome(state, new_state, action_probabilities, reward, done, value, next_value)
 			else:
 				agent.store_outcome(state, new_state, action_probabilities, reward, done)			
-
-			train_reward += reward
+			
 			state = new_state
+			train_reward += reward
+			
 
-		if (episode + 1) % args.update_every == 0:
-			loss = agent.update_policy()
-		
+		episodes_returns.append(train_reward)
+		agent.update_policy()
 		
 		if (episode + 1) % args.print_every == 0:
 			print('Training episode:', episode + 1)
 			print('Episode return:', train_reward)
+		if (episode+1) % 100 == 0:
+			print("fatto")
+		
 		"""	
 		Intermediate model saving
 			checkpoint_filename = f"modelnobs_checkpoint_ep{episode + 1}.mdl"
@@ -85,7 +82,10 @@ def main():
 			print(f"Modello salvato in {checkpoint_filename}")
 		"""
 
-	torch.save(agent.policy.state_dict(), "modelActorCritic.mdl")
+	with open("training_statsBaseline.pkl", "wb") as f:
+		pickle.dump({"returns": episodes_returns,}, f)
+
+	torch.save(agent.policy.state_dict(), "modelBaseline.mdl")
 	env.close()
 	
 

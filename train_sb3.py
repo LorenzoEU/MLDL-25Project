27d27840587e92ff4dad844d644273923cd04816
Lorenzo Durante <103606632+LorenzoEU@users.noreply.github.com>
2025-05-03 21:32:49 +0200

    Update train_sb3.py

--- train_sb3.py
+++ train_sb3.py
@@ -6,10 +6,12 @@
 """
 import gym
 from env.custom_hopper import *
+from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
 
 def main():
     train_env = gym.make('CustomHopper-source-v0')
-
+    timesteps = 1000000
     print('State space:', train_env.observation_space)  # state-space
     print('Action space:', train_env.action_space)  # action-space
     print('Dynamics parameters:', train_env.get_parameters())  # masses of each link of the Hopper
@@ -17,6 +19,11 @@ def main():
     #
     # TASK 4 & 5: train and test policies on the Hopper env with stable-baselines3
     #
+    model = PPO("MlpPolicy", train_env, verbose = 1, gamma=0.99, n_steps=4096, batch_size=128, learning_rate=0.0003)
+    model.learn(total_timesteps=timesteps)
+    model.save("ppo_hopper")
+    mean_reward, std_reward = evaluate_policy(model, train_env, n_eval_episodes=100)
+    print(f"Mean reward: {mean_reward:.2f} +/- {std_reward:.2f}")
 
 if __name__ == '__main__':
     main()
